ef30deaf42b6a2aca6c2d605b6cffbe555ca30e0
Lamine Diamoutene <pridila.2006@gmail.com>
2025-03-05 01:25:48 +0200

    remove delete button

--- src/app/watch/page.tsx
+++ src/app/watch/page.tsx
@@ -50,14 +50,6 @@ const WatchPage = async ({ searchParams }: Props) => {
         </Card>
 
         <ul className="grid grid-cols-[repeat(auto-fill,minmax(6rem,1fr))] flex-wrap gap-2">
-          {extracted.type === "direct" && (
-            <Button asChild variant={"outline"} size={"sm"}>
-              <a href={extracted.url} download={true}>
-                Telecharger
-              </a>
-            </Button>
-          )}
-
           {players.map((item, index) => (
             <Button
               asChild
